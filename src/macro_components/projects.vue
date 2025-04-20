5e8a36403f912939baa2228cb0210811b7f83d98
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-20 10:49:33 -0300

    .

--- src/macro_components/projects.vue
+++ src/macro_components/projects.vue
@@ -3,13 +3,21 @@
 </script>
 
 <template>
+    <div>
+        <PJ urlimg="../src/assets/img/B2CA.png" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
+        <PJ urlimg="../src/assets/img/carwash.png" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
+        <PJ></PJ>
+    </div>
 
-    <PJ></PJ>
-    <PJ></PJ>
-    <PJ></PJ>
 
 </template>
 
-<script scoped>
 
-</script>
+<style scoped>
+    div{
+        margin-top: 2rem;
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+    }
+</style>
--- src/micro_components/project.vue
+++ src/micro_components/project.vue
@@ -1,19 +1,43 @@
 <script setup>
-    
+    const props = defineProps({
+        urlimg: String,
+        title: String,
+        resume: String
+    })
 </script>
 
 <template>
-    <div>
-        <img src="" alt="" srcset="">
-        <title></title>
-        <p></p>
+    <a href="https://chipper-gumdrop-9088d1.netlify.app/">
+        <img :src="urlimg" alt="" srcset="">
+        <title>{{ title }}</title>
+        <p>{{ resume }}</p>
         <div id="tecnologias"></div>
-    </div>
+    </a>
     
     
 
 </template>
 
-<script scoped>
+<style scoped>
+    a{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto auto;
+        text-decoration: none;
+        color: white;
+        gap: 1rem;
 
-</script>
+        img{
+            
+            align-self: center;
+            width: auto;
+        }
+
+        p{
+            grid-column: 1 / span 2;
+        }
+
+
+    }
+
+</style>
