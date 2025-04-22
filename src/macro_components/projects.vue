6b692af1ee73c5915847b746df2bdde2f1d774b5
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-22 11:45:08 -0300

    Update projects.vue

--- src/macro_components/projects.vue
+++ src/macro_components/projects.vue
@@ -1,12 +1,15 @@
 <script setup>
     import PJ from "../micro_components/project.vue"
+    import b2ca from '../assets/img/B2CA.png'
+    import carwash from '../assets/img/carwash.png'
+    import lux from '../assets/img/luxChaffeur.png'
 </script>
 
 <template>
     <div id="projects">
-        <PJ urlsite="https://chipper-gumdrop-9088d1.netlify.app/" urlimg="../assets/img/B2CA.png" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
-        <PJ urlsite="https://projeto-car-wash.vercel.app" urlimg="../assets/img/carwash.png" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
-        <PJ urlsite="https://projeto-lux-chaffeur-sass.vercel.app" urlimg="../assets/img/luxChaffeur.png" title="Projeto LuxChaffeur" resume="LandPage de empresa de motorista particular, desenvolvida com Html, Css,  Javascript e préprocessador SASS."></PJ>
+        <PJ urlsite="https://chipper-gumdrop-9088d1.netlify.app/" :urlimg="b2ca" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
+        <PJ urlsite="https://projeto-car-wash.vercel.app" :urlimg="carwash" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
+        <PJ urlsite="https://projeto-lux-chaffeur-sass.vercel.app" :urlimg="lux" title="Projeto LuxChaffeur" resume="LandPage de empresa de motorista particular, desenvolvida com Html, Css,  Javascript e préprocessador SASS."></PJ>
     </div>
 
 
