020c15063a013ecc2fef954157d45cb468f59b91
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-22 11:23:43 -0300

    .

--- src/macro_components/projects.vue
+++ src/macro_components/projects.vue
@@ -4,9 +4,9 @@
 
 <template>
     <div id="projects">
-        <PJ urlsite="https://chipper-gumdrop-9088d1.netlify.app/" urlimg="/src/assets/img/B2CA.png" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
-        <PJ urlsite="https://projeto-car-wash.vercel.app" urlimg="/src/assets/img/carwash.png" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
-        <PJ urlsite="https://projeto-lux-chaffeur-sass.vercel.app" urlimg="/src/assets/img/luxChaffeur.png" title="Projeto LuxChaffeur" resume="LandPage de empresa de motorista particular, desenvolvida com Html, Css,  Javascript e préprocessador SASS."></PJ>
+        <PJ urlsite="https://chipper-gumdrop-9088d1.netlify.app/" urlimg="src/assets/img/B2CA.png" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
+        <PJ urlsite="https://projeto-car-wash.vercel.app" urlimg="src/assets/img/carwash.png" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
+        <PJ urlsite="https://projeto-lux-chaffeur-sass.vercel.app" urlimg="src/assets/img/luxChaffeur.png" title="Projeto LuxChaffeur" resume="LandPage de empresa de motorista particular, desenvolvida com Html, Css,  Javascript e préprocessador SASS."></PJ>
     </div>
 
 
