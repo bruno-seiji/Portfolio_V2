98e6d559e911017f43dcbdbf91c2c9f57f665f02
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-22 14:40:23 -0300

    Update projects.vue

--- src/macro_components/projects.vue
+++ src/macro_components/projects.vue
@@ -3,6 +3,7 @@
     import b2ca from '../assets/img/B2CA.png'
     import carwash from '../assets/img/carwash.png'
     import lux from '../assets/img/luxChaffeur.png'
+    import barba from '../assets/img/barbearia.png'
 </script>
 
 <template>
@@ -10,6 +11,7 @@
         <PJ urlsite="https://chipper-gumdrop-9088d1.netlify.app/" :urlimg="b2ca" title="B2CA - Soluções em Tecnologia" resume="Landpage de uma empresa comercial de solu;óes em T.I, desenvolvido utilizando tecnologias como Vue.js, afim de estudar tal tecnologia."></PJ>
         <PJ urlsite="https://projeto-car-wash.vercel.app" :urlimg="carwash" title="Mobile Detaling " resume="LandPage de uma empresa lavagem de carros, desenvolvida para estudo de tecnologias como TailWindCss, Html e CSS" ></PJ>
         <PJ urlsite="https://projeto-lux-chaffeur-sass.vercel.app" :urlimg="lux" title="Projeto LuxChaffeur" resume="LandPage de empresa de motorista particular, desenvolvida com Html, Css,  Javascript e préprocessador SASS."></PJ>
+        <PJ urlsite="https://barbearia-alura-bscc.netlify.app" :urlimg="barba" title="Projeto Barbearia" resume="LandPage de uma babrbearia, desenvolvida com Html, Css,  Javascript."></PJ>
     </div>
 
 
